d123a72c7060cbb48e7c7247d81a53e37a35f319
Edmund Wong Li Hong <alternativeley@gmail.com>
2026-01-19 22:21:14 +0800

    Set up admin functions

--- src/index.js
+++ src/index.js
@@ -1,26 +1,47 @@
-require('dotenv').config();
-const { Telegraf } = require('telegraf');
+const newEventWizard = require('./ops/newEvent');
+const editEventWizard = require('./ops/editEvent');
+const eventSummaryWizard = require('./ops/eventSummary');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-// Basic middleware
+const stage = new Scenes.Stage([newEventWizard, editEventWizard, eventSummaryWizard]);
+
+// Middleware
+bot.use(session());
+bot.use(stage.middleware());
+
+// Basic logger
 bot.use(async (ctx, next) => {
-  console.log(`Update ${ctx.update.update_id} received`);
-  return await next();
+    console.log(`Update ${ctx.update.update_id} received`);
+    return await next();
 });
 
 // Commands
-bot.start((ctx) => ctx.reply('Welcome! I am your new Telegraf bot.'));
-bot.help((ctx) => ctx.reply('Send me a message or use commands like /start.'));
+bot.command('newevent', (ctx) => ctx.scene.enter('NEW_EVENT_WIZARD'));
+bot.command('editevent', (ctx) => ctx.scene.enter('EDIT_EVENT_WIZARD'));
+bot.command('eventsummary', (ctx) => ctx.scene.enter('EVENT_SUMMARY_WIZARD'));
+bot.start((ctx) => ctx.reply('Welcome! Use /newevent to create a session, /editevent to modify one, or /eventsummary to view signups.'));
+bot.help((ctx) => ctx.reply('Available commands:\n/newevent - Create a new event\n/editevent - Edit an existing event\n/eventsummary - View signup counts'));
 
-// Basic text response
-bot.on('text', async (ctx) => {
-  await ctx.reply(`You said: ${ctx.message.text}`);
+// Unknown command handler
+bot.on('text', (ctx) => {
+    if (ctx.message.text.startsWith('/')) {
+        return ctx.reply('Unknown command. Please run /help for a list of commands and their purpose.');
+    }
+    return ctx.reply('I\'m not sure I understand. Type /help to see what I can do.');
 });
 
-// Launch bot
-bot.launch().then(() => {
-  console.log('Bot is running...');
+// Launch bot using webhooks
+const webhookUrl = `${process.env.WEBHOOK_DOMAIN}/bot${process.env.BOT_TOKEN}`;
+
+bot.launch({
+    webhook: {
+        domain: process.env.WEBHOOK_DOMAIN,
+        secretToken: process.env.WEBHOOK_SECRET_TOKEN,
+        port: process.env.PORT || 3000,
+    },
+}).then(() => {
+    console.log(`Bot is running on ${webhookUrl} (Webhook mode)`);
 });
 
 // Enable graceful stop
